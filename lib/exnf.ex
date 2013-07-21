e6ff06c2c289fa26f4a8774f56cf667d574c5234
EC2 Default User <ec2-user@ip-10-40-65-192.ec2.internal>
2013-07-21 02:42:10 +0000

    fixed add remove ignore on single host

--- lib/exnf.ex
+++ lib/exnf.ex
@@ -19,7 +19,7 @@ defmodule Exnf do
     start("?",[])
   end
   def start(_type,_args) do
-    start_link([poll_interval: 500,strategy: :file,mode: :default])
+    start_link([poll_interval: 200,strategy: :file,mode: :default])
   end
   def master_name do
     "exnf_master@#{:net_adm.localhost}"  |> binary_to_atom
@@ -93,20 +93,50 @@ defmodule Exnf do
     :timer.sleep(2000)
     :gen_server.call :exnf, :stop
   end
-  def loop_poll do
-    config = get_config
-    nodes = Enum.uniq(get_nodes ++ Node.list)
-    results = Enum.map(nodes,ping(&1))
+  def sp do
+    start_loop_poll
+  end
+  def start_loop_poll do
+    Lager.info "Exnf.start_loop_poll: #{inspect get_nodes}"
+    :gen_server.cast :exnf, :loop_poll
+  end
+  def poll(poll_interval) do
+    #Lager.info "Poll SPAWN: #{inspect self}"
+    :timer.sleep(poll_interval)
+    :gen_server.cast :exnf, :loop_poll
+  end
+  def handle_cast( :loop_poll, {config, nodes}) do
+    new_nodes = Enum.uniq(nodes ++ Node.list)
+    results = Enum.map(new_nodes,ping(&1,nodes))
     pi = config[:poll_interval]
     #Lager.info "nodes #{inspect nodes}\nconfig #{inspect config}\npi #{inspect pi}\nresults #{inspect results}"
+    results = Enum.filter(results,fn({key,val}) -> key != :ignore end)
+    if ((Enum.count(results)) > 0) do
+      Lager.info "Node list was: #{inspect new_nodes}"
+      :gen_server.cast(:exnf,{:updates,results}) 
+    end
     if (is_number(pi)) do
       #Lager.info "Exnf.poll: sleeping for #{pi}"
-      :timer.sleep(pi)
-      loop_poll
+      spawn(__MODULE__,:poll,[config[:poll_interval]])
     else
       Lager.info "Exnf.poll Exiting polling loop"
     end
+    {:noreply,{config,new_nodes}}
   end
+  def handle_cast({:updates,results},{config,nodes}) when results != [nil] do
+    Lager.info "updates #{inspect results}" 
+    reduced = Enum.reduce(results,[add: [], remove: [],ignore: []],fn({key,val},acc) -> ListDict.put(acc,key,[val|acc[key]]) end)
+    Lager.info "reduced: #{inspect reduced}"
+    spawn(__MODULE__,:do_updates,[reduced])
+    {:noreply,{config,nodes}}
+  end
+  def do_updates(updates) when is_list(updates) do
+    adds = updates[:add]
+    Enum.map(adds,fn (node) -> add(node) end)
+    removes = updates[:remove]
+    Enum.map(removes,fn (node) -> remove(node) end)
+    ignores = updates[:ignore] 
+  end 
   def stop do
     :gen_server.call :exnf, :stop
   end
@@ -138,23 +168,28 @@ defmodule Exnf do
   def handle_call({:update_config,new_config},_from,{config,nodes}) do
     {:reply,new_config,{new_config,nodes}}
   end
-  def ping(node) do
+  def ping(node,nodes) do
+     action = :ignore
      case :net_adm.ping(node) do
       :pang -> 
         if (node != master_name) do
           Lager.info "Exnf.ping: removing node #{inspect node}" 
-          remove(node)  
-          :false
+          action =  :remove
         else
           Lager.info "master node appears down"
-          :false
+          action = :ignore
         end
-      :pong -> unless node in get_nodes do
+      :pong -> unless node in nodes do
           Lager.info "Exnf.ping: adding node #{inspect node}"
-          add(node)
+          action = :add
         end
+      :pong -> 
+        Lager.info "Exnf.ping: pong ignoring #{inspect node}"
+        action = :ignore
       doh -> Lager.error "something fucked up #{doh}"
     end
+    #Lager.info "Ping: action : #{action} node #{node}"
+    {action,node}
 
   end 
   def get_config do
